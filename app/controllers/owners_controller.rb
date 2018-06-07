d4566450785d049f564381ada71caa822e50aa72
Alberto Carreras <a.carreras.c@gmail.com>
2018-06-06 21:40:40 -0400

    Solved lab

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,29 +1,41 @@
 class OwnersController < ApplicationController
-
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    unless params['pet']['name'].empty?
+      @owner.pets << Pet.create(name: params['pet']['name'])
+      @owner.save # saves to the database
+    end
+    redirect "owners/#{@owner.id}"
+  end
+
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
-  end
-
-  get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
-    erb :'/owners/edit'
-  end
-
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  post '/owners/:id' do 
-   
+  post '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    @owner.update(params['owner'])
+    unless params['pet']['name'].empty?
+      @owner.pets << Pet.create(name: params['pet']['name'])
+    end
+    redirect to "owners/#{@owner.id}"
   end
+
+  get '/owners/:id/edit' do
+    @owner = Owner.find(params[:id])
+    erb :'/owners/edit'
+  end
+
+
 end
